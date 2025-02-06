6c49a5e5014110845ece1474ed79bb8941329c2e
C-Tharun <116745984+C-Tharun@users.noreply.github.com>
2025-02-06 21:36:43 +0530

    Initial commit

---
+++ client/src/app/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Search, SunDim} from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between bg-white px-4 py-3 dark:bg-black'>
+      {/* Search Bar */}
+      <div className='flex items-center gap-8'>
+        <div className='relative flex h-min w-[200px]'>
+          <Search className='absloute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white' />
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+SunDim.displayName = 'SunDim';
+as
