8b78d1dd58fef16d56b268fda2579263d2239a86
C-Tharun <116745984+C-Tharun@users.noreply.github.com>
2025-04-02 17:39:33 +0530

    ServerModifications

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: "Project_Management_Dashboard",
+        script: "npm",
+        args: "run dev",
+        env: {
+          NODE_ENV: "development",
+        },
+      },
+    ],
+  };
