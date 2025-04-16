e477d8a66cede9e3f5a313f97db9f1724f05c821
C-Tharun <116745984+C-Tharun@users.noreply.github.com>
2025-04-16 23:20:57 +0530

    port changed back to 3000

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,7 @@ app.use("/teams", teamRoutes);
 
 
 /* SERVER */
-const port = Number(process.env.PORT) || 8080;
+const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
